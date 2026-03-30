2e11fde4a6cf1073405595f16a38b9bf7d5ab990
joaoluizz <joao.l.neto19@aluno.senai.br>
2026-03-30 11:55:20 -0300

    aula 01 C

---
+++ aula_01/ex_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int ano;
+    
+    printf("Digite seu ano de nascimento: ");
+    scanf("%i", &ano);
+    
+    printf("Você tem %i anos: ", 2026 - ano);
+    
+}
